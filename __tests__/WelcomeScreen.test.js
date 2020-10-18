02a425120ce3c9bec6b532a11205120859e0078c
lea <lea.franz@edu.fh-joanneum.at>
2020-10-18 12:03:48 +0200

    added tests

---
+++ __tests__/WelcomeScreen.test.js
@@ -0,0 +1,14 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+it('renders correctly', () => {
+  renderer.create(<WelcomeScreen />);
+});
+
+it('matches snapshot', () => {
+    const tree = renderer.create().toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+  
