eb0ab54ccd73871a1ff07df8b9438a0f72093418
Lea Franz <lea.franz@edu.fh-joanneum.at>
2020-10-24 16:00:22 +0200

    fixed tests

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -4,6 +4,10 @@ import App from '../App';
 
 import renderer from 'react-test-renderer';
 
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
