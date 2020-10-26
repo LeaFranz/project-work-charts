4d53eed55f9685f3a3bb3982b13c6978db41f1f4
Lea Franz <lea.franz@edu.fh-joanneum.at>
2020-10-26 13:51:54 +0100

    fixed tests

--- __tests__/LoginScreen.test.js
+++ __tests__/LoginScreen.test.js
@@ -1,17 +1,10 @@
-// import 'react-native';
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import LoginScreen from '../screens/LoginScreen';
+import 'react-native';
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import LoginScreen from '../screens/LoginScreen';
 
-// beforeEach(() => {
-//   jest.useFakeTimers();
-// });
-
-// it('renders correctly', () => {
-//   //renderer.create(<LoginScreen />);
-// });
-
-// it('matches snapshot', () => {
-//   // const tree = renderer.create().toJSON();
-//   // expect(tree).toMatchSnapshot();
-// });
+it('renders correctly', () => {
+  const jsx = <LoginScreen />;
+  const renderer = render(jsx);
+  expect(renderer.toJSON()).toMatchSnapshot();
+});
